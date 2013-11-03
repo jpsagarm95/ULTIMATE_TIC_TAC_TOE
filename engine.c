4a2fa7e15bcf276b2e76143368f4dee3d5ed5228
Tejaswini Ganji <tejudec5@gmail.com>
2013-11-03 10:59:36 +0530

    third commit

--- engine.c
+++ engine.c
@@ -1,72 +1,97 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-/***************************************************************
-** engine func which returns the winner of the game 1 or 2
-** @param board the pointer to the board
-** @return :it returns winner(player_no) of the game :1 or 2
-** @tejaswini 
-****************************************************************/
+int box_win(int,int,char****,int);
+int w_win(int **,int);
+void print(FILE*,char ****);
+void print_as_two_by_two_matrix(fp,int,char ****);
+void scan(FILE *,char ****);
+void to_scan(FILE *,int,char ****);
 
-int engine(int ****board)
+int main()
 {
-	static int i,j,player1_win=0,player2_win=0,x1,y1,x2=-1,y2=-1,by_player1,by_player2;
-	
-	//'w'is a winning 3x3 matrix, which keeps track of the grids won by players
-	int **w=(int**)malloc(sizeof(int*)*3);
+	FILE* fp=fopen("bot.text","r+");
+	int i,j,k,player_1=0;player_2=0,x2=-1,y2=-1,x,y;
+	int **win=(int**)malloc(sizeof(int*)*3);
 	for(i=0;i<3;i++)
-	int w[i]=(int*)malloc(sizeof(int)*3);
+	win[i]=(int*)malloc(sizeof(int)*3);
 	
-	//initializing all elements of w to -1
 	for(i=0;i<3;i++)
 	{
 		for(j=0;j<3;j++)
-		w[i][j]=-1;
+		win[i][j]=-1;
 	}
 	
-	//a while loop which runs until the game is won by anyone of the players
+	char ****board=(char****)malloc(sizeof(char***)*3);
+	for(i=0;i<3;i++)
+	{
+		*win[i]=(char***)malloc(sizeof(char**)*3);
+		for(j=0;j<3;j++)
+		{
+			*win[i][j]=(char**)malloc(sizeof(char*)*3);
+			for(k=0;k<3;k++)
+			win[i][j][k]=(char*)malloc(sizeof(char)*3);
+		}
+	}
+	
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j>3;j++)
+		{
+			for(k=0;k<3;k++)
+			{
+				for(l=0;l<3;l++)
+				board[i][j][k][l]='.';
+			}
+		}
+	}
+	
 	while(player1_win==0 && player2_win==0)
 	{
 		//print to 1st player
-		print(board);
+		print(fp,board);//check..........
+		fprintf(fp,"%d %d\n",x2,y2);
 		
-		sleep(2);
-        system("clear");
+		system("./bot1");
 		
-		by_player1=monster(board,x2,y2,w);
+		scan(fp,board);
+		fscanf(fp,"%d %d",&x,&y);
 		
-		//decoding the x,y coordinates of grid of board marked by player2
-		x1=by_player1/3;y1=by_player1%3;
+		if(win[x][y]!=-1){x1=-1;y1=-1;}
+		else {x1=x;y1=y;}
 		
 		//a check to see whether any of the grids is won by player1 and mark that
 		//particular element in 'w'matrix as '1'
-		if(w[x1][y1]==-1 && box_win(x1,y1,board,1)==1)w[x1][y1]=1;
+		if(win[x][y]==-1 && box_win(x1,y1,board,1)==1)win[x1][y1]=1;
 		
 		//to check whether player1 is won 
 		if(w_win(w,1)==1){player1_win=1;break;}
 		
 		//print to 2nd player
-		print(board);
+		print(fp,board);//check........
+		fprintf(fp,"%d %d\n",x1,y1);
 		
-		sleep(2);
-        system("clear");
+		system("./bot2");
 		
-		by_player2=monster(board,x1,y1,w);
+		scan(fp,board);
+		fscanf(fp,"%d %d",&x,&y);
 		
-		//decoding the x,y coordinates of grid of board marked by player1
-		x2=by_player2/3;y2=by_player2%3;
+		if(win[x][y]!=-1){x2=-1;y2=-1;}
+		else {x2=x;y2=y;}
 		
 		//a check to see whether any of the grids is won by player2 and mark that
 		//particular element in 'w'matrix as '2'
-		if(w[x2][y2]==-1 && box_win(x2,y2,board,2)==2)w[x2][y2]=2;
+		if(w[x][y]==-1 && box_win(x2,y2,board,2)==2)w[x2][y2]=2;
 		
 		//to check whether player2 is won
 		if(w_win(w,2)==2){player2_win=1;break;}
 	}
+	fclose(fp);
+	
 	if(player1_win==1)return 1;
 	else if(player2_win==1)return 2;
 }
-	
-	
+
 /****************************************************************************************
 ** box_win func to maintain 'w' matrix
 ** @param x,y : coordinates of the grid in the board
@@ -74,9 +99,11 @@ int engine(int ****board)
 ** @player_no:player 1 or 2
 ** @return:returns the current player_no if that grid is won by the current player,else 0
 ****************************************************************************************/
-int box_win(int x,int y,int ****board,int player_no)
+int box_win(int x,int y,char ****board,int player_no)
 {
-	int a=player_no;
+	char a;
+	if(player_no==1)a='X';
+	else {a='O'};
 	if((board[x][y][0][0]==a && board[x][y][0][1]==a && board[x][y][0][2]==a)||
 	   (board[x][y][1][0]==a && board[x][y][1][1]==a && board[x][y][1][2]==a)||
 	   (board[x][y][2][0]==a && board[x][y][2][1]==a && board[x][y][2][2]==a)||
@@ -85,7 +112,7 @@ int box_win(int x,int y,int ****board,int player_no)
 	   (board[x][y][0][2]==a && board[x][y][1][2]==a && board[x][y][2][2]==a)||
 	   (board[x][y][0][0]==a && board[x][y][1][1]==a && board[x][y][2][2]==a)||
 	   (board[x][y][0][2]==a && board[x][y][1][1]==a && board[x][y][2][0]==a))
-	 return a;
+	 return player_no;
 	 else return 0;
 }
 
@@ -115,122 +142,130 @@ int w_win(int **w,int player_no)
 ** @param pointer to board
 ** prints whole board as 2x2 matrix
 ****************************************************************************/	
-void print(int ****board)
+void print(FILE *fp,char ****board)
 {
-	print_as_two_by_two_matrix(0,board);
-	print_as_two_by_two_matrix(1,board);
-	print_as_two_by_two_matrix(2,board);
+	print_as_two_by_two_matrix(fp,0,board);
+	print_as_two_by_two_matrix(fp,1,board);
+	print_as_two_by_two_matrix(fp,2,board);
 	
 }
 
-/******************************************************************************
-**sub function which helps print function to print the board
+
+/********************************************************************************
+**sub function which helps print function to print the board to file and terminal
+** @param fp:pointer to FILE
 ** @param x1:the first x coordinate of the board(the x coordinate of the grid)
 ** @param board :the pointer to the board
 ** prints all the elements in the x1 th row of the grid as required 2x2 matrix
-******************************************************************************/	
-void print_as_two_by_two_matrix(int x1,int ****board)
+********************************************************************************/	
+void print_as_two_by_two_matrix(FILE *fp,int x1,char ****board)
 {
 	int i;
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][0][0][i]==-1)
-	printf("|___| ");
-	else if(board[x1][0][0][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][0][0][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][0][0][i]);
+	printf("%c "board[x1][0][0][i]);
 	}
-	
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][1][0][i]==-1)
-	printf("|___| ");
-	else if(board[x1][1][0][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][1][0][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][1][0][i]);
+	printf("%c "board[x1][1][0][i]);
 	}
-	
-	
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][2][0][i]==-1)
-	printf("|___| ");
-	else if(board[x1][2][0][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][2][0][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][2][0][i]);
+	printf("%c "board[x1][2][0][i]);
 	}
-	
+	fprintf(fp,"\n");
 	printf("\n");
 	
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][0][1][i]==-1)
-	printf("|___| ");
-	else if(board[x1][0][1][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][0][1][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][0][1][i]);
+	printf("%c "board[x1][0][1][i]);
 	}
-	
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][1][1][i]==-1)
-	printf("|___| ");
-	else if(board[x1][1][1][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][1][1][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][1][1][i]);
+	printf("%c "board[x1][1][1][i]);
 	}
-
-	
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][2][1][i]==-1)
-	printf("|___| ");
-	else if(board[x1][2][1][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][2][1][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][2][1][i]);
+	printf("%c "board[x1][2][1][i]);
 	}
-	
+	fprintf(fp,"\n");
 	printf("\n");
 	
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][0][2][i]==-1)
-	printf("|___| ");
-	else if(board[x1][0][2][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][0][2][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][0][2][i]);
+	printf("%c "board[x1][0][2][i]);
 	}
-
-	
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][1][2][i]==-1)
-	printf("|___| ");
-	else if(board[x1][1][2][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][1][2][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][1][2][i]);
+	printf("%c "board[x1][1][2][i]);
 	}
-
-	
 	for(i=0;i<3;i++)
 	{
-	if(board[x1][2][2][i]==0)
-	printf("|___| ");
-	else if(board[x1][2][2][i]==1)
-	printf("|_x_| ");
-	else if(board[x1][2][2][i]==2)
-	printf("|_o_| ");
+	fprintf(fp,"%c ",board[x1][2][2][i]);
+	printf("%c "board[x1][2][2][i]);
 	}
-
-	
+	fprintf(fp,"\n");
 	printf("\n");
 }
-		
+
+/****************************************************************************
+** func scan to scan the whole board as 2x2 matrix from file.
+** @param pointer to board and FILE -fp
+** scans whole board as 2x2 matrix
+****************************************************************************/
+void scan(FILE *fp,char ****board)
+{
+	to_scan(fp,0,board);
+	to_scan(fp,1,board);
+	to_scan(fp,2,board);
+	
+}
+
+
+/******************************************************************************
+**sub function which helps scan function to scan the board
+** @param fp:FILE pointer
+** @param x1:the first x coordinate of the board(the x coordinate of the grid)
+** @param board :the pointer to the board
+** scans all the elements in the x1 th row of the grid as required 2x2 matrix
+******************************************************************************/	
+void to_scan(FILE *fp,int x1,char ****board)
+{
+	int i;
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][0][0][i]);
+	
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][1][0][i]);
+	
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][2][0][i]);
+	
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][0][1][i]);
+	
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][1][1][i]);
+	
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][2][1][i]);
+	
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][0][2][i]);
+	
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][1][2][i]);
+	
+	for(i=0;i<3;i++)
+	fscanf(fp," %c",&board[x1][2][2][i]);
+	
+}
+			
